6988948d4ea061722095325926cf58612a52093e
Evan Hall <ebhall99@gmail.com>
2020-09-10 18:06:58 -0400

    Finished writing twists and test cases

--- src/main/kotlin/com/millibyte1/cubesearch/cube/Cube.kt
+++ src/main/kotlin/com/millibyte1/cubesearch/cube/Cube.kt
@@ -11,15 +11,25 @@ import com.millibyte1.cubesearch.cube.Twist
  *
  * @constructor constructs cube from a copy of [data]
  * @param data the 6x9 array representing the desired cube. Format: (front, back, left, right, up, down)
+ *
+ * Flattened cube:
+ *
+ *         0 1 2
+ *         3 U 5
+ *         6 7 8
+ *  0 1 2  0 1 2  0 1 2  0 1 2
+ *  3 L 5  3 F 5  3 R 5  3 B 5
+ *  6 7 8  6 7 8  6 7 8  6 7 8
+ *         0 1 2
+ *         3 D 5
+ *         6 7 8
  */
-//TODO(tuples): switch from arrays to tuples, would be inefficient to check validity with built-in arrays
+//TODO(factories) add factory functions and make constructor private so that testing lengths in code is unnecessary
 class Cube constructor(data: Array<IntArray>) : Twistable, Serializable {
 
     val data: Array<IntArray>
 
     init {
-        //extension function to deep copy array
-        fun Array<IntArray>.copy() = Array(size) { it -> get(it).clone() }
         this.data = data.copy()
     }
 
@@ -37,13 +47,418 @@ class Cube constructor(data: Array<IntArray>) : Twistable, Serializable {
      * @return the cube that results from applying this twist
      */
     override fun twist(twist: Twist): Twistable {
-        //TODO(tuples): switch to tuples then implement twists
+        return when(twist) {
+            Twist.FRONT_90 -> twistFront90()
+            Twist.FRONT_180 -> twistFront180()
+            Twist.FRONT_270 -> twistFront270()
+            Twist.BACK_90 -> twistBack90()
+            Twist.BACK_180 -> twistBack180()
+            Twist.BACK_270 -> twistBack270()
+            Twist.LEFT_90 -> twistLeft90()
+            Twist.LEFT_180 -> twistLeft180()
+            Twist.LEFT_270 -> twistLeft270()
+            Twist.RIGHT_90 -> twistRight90()
+            Twist.RIGHT_180 -> twistRight180()
+            Twist.RIGHT_270 -> twistRight270()
+            Twist.UP_90 -> twistUp90()
+            Twist.UP_180 -> twistUp180()
+            Twist.UP_270 -> twistUp270()
+            Twist.DOWN_90 -> twistDown90()
+            Twist.DOWN_180 -> twistDown180()
+            Twist.DOWN_270 -> twistDown270()
+        }
+    }
+    private fun twistFront90() : Twistable {
+        val copy = data.copy()
+        //left face update
+        copy[2][8] = data[5][2]
+        copy[2][5] = data[5][1]
+        copy[2][2] = data[5][0]
+        //up face update
+        copy[4][6] = data[2][8]
+        copy[4][7] = data[2][5]
+        copy[4][8] = data[2][2]
+        //right face update
+        copy[3][0] = data[4][6]
+        copy[3][3] = data[4][7]
+        copy[3][6] = data[4][8]
+        //down face update
+        copy[5][2] = data[3][0]
+        copy[5][1] = data[3][3]
+        copy[5][0] - data[3][6]
+        //wrap and return
+        return Cube(copy)
+    }
+    private fun twistFront180() : Twistable {
+        val copy = data.copy()
+        //left face update
+        copy[2][8] = data[3][0]
+        copy[2][5] = data[3][3]
+        copy[2][2] = data[3][6]
+        //up face update
+        copy[4][6] = data[5][2]
+        copy[4][7] = data[5][1]
+        copy[4][8] = data[5][0]
+        //right face update
+        copy[3][0] = data[2][8]
+        copy[3][3] = data[2][5]
+        copy[3][6] = data[2][2]
+        //down face update
+        copy[5][2] = data[4][6]
+        copy[5][1] = data[4][7]
+        copy[5][0] = data[4][8]
+        //wrap and return
+        return Cube(copy)
+    }
+    private fun twistFront270() : Twistable {
+        val copy = data.copy()
+        //left face update
+        copy[2][8] = data[4][6]
+        copy[2][5] = data[4][7]
+        copy[2][2] = data[4][8]
+        //up face update
+        copy[4][6] = data[3][0]
+        copy[4][7] = data[3][3]
+        copy[4][8] = data[3][6]
+        //right face update
+        copy[3][0] = data[5][2]
+        copy[3][3] = data[5][1]
+        copy[3][6] = data[5][0]
+        //down face update
+        copy[5][2] = data[2][8]
+        copy[5][1] = data[2][5]
+        copy[5][0] = data[2][2]
+        //wrap and return
+        return Cube(copy)
+    }
+    private fun twistBack90() : Twistable {
+        val copy = data.copy()
+        //right face update
+        copy[3][2] = data[5][8]
+        copy[3][5] = data[5][7]
+        copy[3][8] = data[5][6]
+        //up face update
+        copy[4][0] = data[3][2]
+        copy[4][1] = data[3][5]
+        copy[4][2] = data[3][8]
+        //left face update
+        copy[2][6] = data[4][0]
+        copy[2][3] = data[4][1]
+        copy[2][0] = data[4][2]
+        //down face update
+        copy[5][8] = data[2][6]
+        copy[5][7] = data[2][3]
+        copy[5][6] = data[2][0]
+        //wrap and return
+        return Cube(copy)
+    }
+    private fun twistBack180() : Twistable {
+        val copy = data.copy()
+        //right face update
+        copy[3][2] = data[2][6]
+        copy[3][5] = data[2][3]
+        copy[3][8] = data[2][0]
+        //up face update
+        copy[4][0] = data[5][8]
+        copy[4][1] = data[5][7]
+        copy[4][2] = data[5][6]
+        //left face update
+        copy[2][6] = data[3][2]
+        copy[2][3] = data[3][5]
+        copy[2][0] = data[3][8]
+        //down face update
+        copy[5][8] = data[4][0]
+        copy[5][7] = data[4][1]
+        copy[5][6] = data[4][2]
+        //wrap and return
+        return Cube(copy)
+    }
+    private fun twistBack270() : Twistable {
+        val copy = data.copy()
+        //right face update
+        copy[3][2] = data[4][0]
+        copy[3][5] = data[4][1]
+        copy[3][8] = data[4][2]
+        //up face update
+        copy[4][0] = data[2][6]
+        copy[4][1] = data[2][3]
+        copy[4][2] = data[2][0]
+        //left face update
+        copy[2][6] = data[5][8]
+        copy[2][3] = data[5][7]
+        copy[2][0] = data[5][6]
+        //down face update
+        copy[5][8] = data[3][2]
+        copy[5][7] = data[3][5]
+        copy[5][6] = data[3][8]
+        //wrap and return
+        return Cube(copy)
+    }
+    private fun twistLeft90() : Twistable {
+        val copy = data.copy()
+        //back face update
+        copy[1][8] = data[5][0]
+        copy[1][5] = data[5][3]
+        copy[1][2] = data[5][6]
+        //up face update
+        copy[4][0] = data[1][8]
+        copy[4][3] = data[1][5]
+        copy[4][6] = data[1][2]
+        //front face update
+        copy[0][0] = data[4][0]
+        copy[0][3] = data[4][3]
+        copy[0][6] = data[4][6]
+        //down face update
+        copy[5][0] = data[0][0]
+        copy[5][3] = data[0][3]
+        copy[5][6] = data[0][6]
+        return Cube(copy)
+    }
+    private fun twistLeft180() : Twistable {
+        val copy = data.copy()
+        //back face update
+        copy[1][8] = data[0][0]
+        copy[1][5] = data[0][3]
+        copy[1][2] = data[0][6]
+        //up face update
+        copy[4][0] = data[5][0]
+        copy[4][3] = data[5][3]
+        copy[4][6] = data[5][6]
+        //front face update
+        copy[0][0] = data[1][8]
+        copy[0][3] = data[1][5]
+        copy[0][6] = data[1][2]
+        //down face update
+        copy[5][0] = data[4][0]
+        copy[5][3] = data[4][3]
+        copy[5][6] = data[4][6]
+        //wrap and return
+        return Cube(copy)
+    }
+    private fun twistLeft270() : Twistable {
+        val copy = data.copy()
+        //back face update
+        copy[1][8] = data[4][0]
+        copy[1][5] = data[4][3]
+        copy[1][2] = data[4][6]
+        //up face update
+        copy[4][0] = data[0][0]
+        copy[4][3] = data[0][3]
+        copy[4][6] = data[0][6]
+        //front face update
+        copy[0][0] = data[5][0]
+        copy[0][3] = data[5][3]
+        copy[0][6] = data[5][6]
+        //down face update
+        copy[5][0] = data[1][8]
+        copy[5][3] = data[1][5]
+        copy[5][6] = data[1][2]
+        return Cube(copy)
+    }
+    private fun twistRight90() : Twistable {
+        val copy = data.copy()
+        //front face update
+        copy[0][8] = data[5][8]
+        copy[0][5] = data[5][5]
+        copy[0][2] = data[5][2]
+        //up face update
+        copy[4][8] = data[0][8]
+        copy[4][5] = data[0][5]
+        copy[4][2] = data[0][2]
+        //back face update
+        copy[1][0] = data[4][8]
+        copy[1][3] = data[4][5]
+        copy[1][6] = data[4][2]
+        //down face update
+        copy[5][8] = data[1][0]
+        copy[5][5] = data[1][3]
+        copy[5][2] = data[1][6]
+        //wrap and return
+        return Cube(copy)
+    }
+    private fun twistRight180() : Twistable {
+        val copy = data.copy()
+        //front face update
+        copy[0][8] = data[1][0]
+        copy[0][5] = data[1][3]
+        copy[0][2] = data[1][6]
+        //up face update
+        copy[4][8] = data[5][8]
+        copy[4][5] = data[5][5]
+        copy[4][2] = data[5][2]
+        //back face update
+        copy[1][0] = data[0][8]
+        copy[1][3] = data[0][5]
+        copy[1][6] = data[0][2]
+        //down face update
+        copy[5][8] = data[1][0]
+        copy[5][5] = data[1][3]
+        copy[5][2] = data[1][6]
+        //wrap and return
+        return Cube(copy)
+    }
+    private fun twistRight270() : Twistable {
+        val copy = data.copy()
+        //front face update
+        copy[0][8] = data[4][8]
+        copy[0][5] = data[4][5]
+        copy[0][2] = data[4][2]
+        //up face update
+        copy[4][8] = data[1][0]
+        copy[4][5] = data[1][3]
+        copy[4][2] = data[1][6]
+        //back face update
+        copy[1][0] = data[5][8]
+        copy[1][3] = data[5][5]
+        copy[1][6] = data[5][2]
+        //down face update
+        copy[5][8] = data[0][8]
+        copy[5][5] = data[0][5]
+        copy[5][2] = data[0][2]
+        //wrap and return
+        return Cube(copy)
+    }
+    private fun twistUp90() : Twistable {
+        val copy = data.copy()
+        //left face update
+        copy[2][2] = data[0][2]
+        copy[2][1] = data[0][1]
+        copy[0][0] = data[0][0]
+        //back face update
+        copy[1][2] = data[2][2]
+        copy[1][1] = data[2][1]
+        copy[1][0] = data[2][0]
+        //right face update
+        copy[3][2] = data[1][2]
+        copy[3][1] = data[1][1]
+        copy[3][0] = data[1][0]
+        //front face update
+        copy[0][2] = data[3][2]
+        copy[0][1] = data[3][1]
+        copy[0][0] = data[3][0]
+        //wrap and return
+        return Cube(copy)
+    }
+    private fun twistUp180() : Twistable {
+        val copy = data.copy()
+        //left face update
+        copy[2][2] = data[3][2]
+        copy[2][1] = data[3][1]
+        copy[0][0] = data[3][0]
+        //back face update
+        copy[1][2] = data[0][2]
+        copy[1][1] = data[0][1]
+        copy[1][0] = data[0][0]
+        //right face update
+        copy[3][2] = data[2][2]
+        copy[3][1] = data[2][1]
+        copy[3][0] = data[2][0]
+        //front face update
+        copy[0][2] = data[1][2]
+        copy[0][1] = data[1][1]
+        copy[0][0] = data[1][0]
+        //wrap and return
+        return Cube(copy)
+    }
+    private fun twistUp270() : Twistable {
+        val copy = data.copy()
+        //left face update
+        copy[2][2] = data[1][2]
+        copy[2][1] = data[1][1]
+        copy[0][0] = data[1][0]
+        //back face update
+        copy[1][2] = data[3][2]
+        copy[1][1] = data[3][1]
+        copy[1][0] = data[3][0]
+        //right face update
+        copy[3][2] = data[0][2]
+        copy[3][1] = data[0][1]
+        copy[3][0] = data[0][0]
+        //front face update
+        copy[0][2] = data[2][2]
+        copy[0][1] = data[2][1]
+        copy[0][0] = data[2][0]
+        //wrap and return
+        return Cube(copy)
+    }
+    private fun twistDown90() : Twistable {
+        val copy = data.copy()
+        //left face update
+        copy[2][6] = data[1][6]
+        copy[2][7] = data[1][7]
+        copy[2][8] = data[1][8]
+        //front face update
+        copy[0][6] = data[2][6]
+        copy[0][7] = data[2][7]
+        copy[0][8] = data[2][8]
+        //right face update
+        copy[3][6] = data[0][6]
+        copy[3][7] = data[0][7]
+        copy[3][8] = data[0][8]
+        //back face update
+        copy[1][6] = data[3][6]
+        copy[1][7] = data[3][7]
+        copy[1][8] = data[3][8]
+        //wrap and return
+        return Cube(copy)
+    }
+    private fun twistDown180() : Twistable {
+        val copy = data.copy()
+        //left face update
+        copy[2][6] = data[3][6]
+        copy[2][7] = data[3][7]
+        copy[2][8] = data[3][8]
+        //front face update
+        copy[0][6] = data[1][6]
+        copy[0][7] = data[1][7]
+        copy[0][8] = data[1][8]
+        //right face update
+        copy[3][6] = data[2][6]
+        copy[3][7] = data[2][7]
+        copy[3][8] = data[2][8]
+        //back face update
+        copy[1][6] = data[0][6]
+        copy[1][7] = data[0][7]
+        copy[1][8] = data[0][8]
+        //wrap and return
+        return Cube(copy)
+    }
+    private fun twistDown270() : Twistable {
+        val copy = data.copy()
+        //left face update
+        copy[2][6] = data[0][6]
+        copy[2][7] = data[0][7]
+        copy[2][8] = data[0][8]
+        //front face update
+        copy[0][6] = data[3][6]
+        copy[0][7] = data[3][7]
+        copy[0][8] = data[3][8]
+        //right face update
+        copy[3][6] = data[1][6]
+        copy[3][7] = data[1][7]
+        copy[3][8] = data[1][8]
+        //back face update
+        copy[1][6] = data[2][6]
+        copy[1][7] = data[2][7]
+        copy[1][8] = data[2][8]
+        //wrap and return
+        return Cube(copy)
     }
 
     /**
      * overridden equality to check whether the cubes have the same configuration
      * pre: cube is
      */
+    override fun equals(other: Any?): Boolean {
+        if(this === other) return true
+        if(other !is Cube) return false
+        other as Cube
+        if(data.contentDeepEquals(other.data)) return true
+        return false
+    }
 
 }
+
+//extension function to deep copy array
+fun Array<IntArray>.copy() = Array(size) { i -> get(i).clone() }
 
--- src/test/kotlin/com/millibyte1/cubesearch/cube/CubeTest.kt
+++ src/test/kotlin/com/millibyte1/cubesearch/cube/CubeTest.kt
@@ -3,6 +3,7 @@ package com.millibyte1.cubesearch.cube
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
 
 import com.millibyte1.cubesearch.cube.Cube
 import com.millibyte1.cubesearch.cube.Twist
@@ -12,40 +13,51 @@ import com.millibyte1.cubesearch.cube.Twist
  */
 class CubeTest {
 
+    //test fixtures
     private fun solved(): Cube {
-        return Cube(arrayOf(
+        return Cube(solvedData())
+    }
+    private fun solvedData(): Array<IntArray> {
+        return arrayOf(
                 IntArray(9) { 0 }, //front face
                 IntArray(9) { 1 }, //back face
                 IntArray(9) { 2 }, //left face
                 IntArray(9) { 3 }, //right face
                 IntArray(9) { 4 }, //up face
                 IntArray(9) { 5 }  //down face
-        ))
+        )
     }
 
+    //unit tests
     @Test
     fun constructors() {
         primaryConstructor()
         copyConstructor()
     }
-
     @Test
     fun primaryConstructor() {
-        //TODO(tuples)
+        //primary constructor will work if the array copy extension function works
+        val array1 = solvedData()
+        val array2 = array1.copy()
+        array1[0][0] = 1
+        assertFalse(array1.contentDeepEquals(array2))
     }
     @Test
     fun copyConstructor() {
+        //asserts that the two cubes are equal but are different objects
         val start = solved()
         val clone = Cube(start)
         assertTrue(start == clone)
-        assertTrue(start !== clone)
+        assertFalse(start === clone)
     }
+
     @Test
     fun immutability() {
         //immutability is dependent on constructors behaving as intended
         constructors()
         val start = solved()
         val clone = Cube(start)
+        //tests
         for(twist in Twist.values()) {
             start.twist(twist)
             assertEquals(start, clone)
@@ -61,29 +73,214 @@ class CubeTest {
         upTwists()
         downTwists()
     }
-
     @Test
     fun frontTwists() {
-        //TODO(tuples)
+        //sets up and tests initial turns
+        val start = solved()
+        var clone1: Twistable = Cube(start)
+        var clone2: Twistable = Cube(start)
+        var clone3: Twistable = Cube(start)
+
+        clone1 = clone1.twist(Twist.FRONT_90)
+        clone2 = clone2.twist(Twist.FRONT_180)
+        clone3 = clone3.twist(Twist.FRONT_270)
+        assertFalse(clone1 == start)
+        assertFalse(clone2 == start)
+        assertFalse(clone3 == start)
+
+        //tests clockwise / F
+        clone1 = clone1.twist(Twist.FRONT_90)
+        assertTrue(clone1 == clone2)
+        clone1 = clone1.twist(Twist.FRONT_90)
+        assertTrue(clone1 == clone3)
+        clone1 = clone1.twist(Twist.FRONT_90)
+        assertTrue(clone1 == start)
+        clone1 = clone1.twist(Twist.FRONT_90)
+        //tests half-turn / F2
+        clone2 = clone2.twist(Twist.FRONT_180)
+        assertTrue(clone2 == start)
+        clone2 = clone2.twist(Twist.FRONT_180)
+        //tests counter-clockwise / F'
+        clone3 = clone3.twist(Twist.FRONT_270)
+        assertTrue(clone3 == clone2)
+        clone3 = clone3.twist(Twist.FRONT_270)
+        assertTrue(clone3 == clone1)
+        clone3 = clone3.twist(Twist.FRONT_270)
+        assertTrue(clone3 == start)
     }
     @Test
     fun backTwists() {
-        //TODO(tuples)
+        //sets up and tests initial turns
+        val start = solved()
+        var clone1: Twistable = Cube(start)
+        var clone2: Twistable = Cube(start)
+        var clone3: Twistable = Cube(start)
+
+        clone1 = clone1.twist(Twist.BACK_90)
+        clone2 = clone2.twist(Twist.BACK_180)
+        clone3 = clone3.twist(Twist.BACK_270)
+        assertFalse(clone1 == start)
+        assertFalse(clone2 == start)
+        assertFalse(clone3 == start)
+
+        //tests clockwise / B
+        clone1 = clone1.twist(Twist.BACK_90)
+        assertTrue(clone1 == clone2)
+        clone1 = clone1.twist(Twist.BACK_90)
+        assertTrue(clone1 == clone3)
+        clone1 = clone1.twist(Twist.BACK_90)
+        assertTrue(clone1 == start)
+        clone1 = clone1.twist(Twist.BACK_90)
+        //tests half-turn / B2
+        clone2 = clone2.twist(Twist.BACK_180)
+        assertTrue(clone2 == start)
+        clone2 = clone2.twist(Twist.BACK_180)
+        //tests counter-clockwise / B'
+        clone3 = clone3.twist(Twist.BACK_270)
+        assertTrue(clone3 == clone2)
+        clone3 = clone3.twist(Twist.BACK_270)
+        assertTrue(clone3 == clone1)
+        clone3 = clone3.twist(Twist.BACK_270)
+        assertTrue(clone3 == start)
     }
     @Test
     fun leftTwists() {
-        //TODO(tuples)
+        //sets up and tests initial turns
+        val start = solved()
+        var clone1: Twistable = Cube(start)
+        var clone2: Twistable = Cube(start)
+        var clone3: Twistable = Cube(start)
+
+        clone1 = clone1.twist(Twist.LEFT_90)
+        clone2 = clone2.twist(Twist.LEFT_180)
+        clone3 = clone3.twist(Twist.LEFT_270)
+        assertFalse(clone1 == start)
+        assertFalse(clone2 == start)
+        assertFalse(clone3 == start)
+
+        //tests clockwise / L
+        clone1 = clone1.twist(Twist.LEFT_90)
+        assertTrue(clone1 == clone2)
+        clone1 = clone1.twist(Twist.LEFT_90)
+        assertTrue(clone1 == clone3)
+        clone1 = clone1.twist(Twist.LEFT_90)
+        assertTrue(clone1 == start)
+        clone1 = clone1.twist(Twist.LEFT_90)
+        //tests half-turn / L2
+        clone2 = clone2.twist(Twist.LEFT_180)
+        assertTrue(clone2 == start)
+        clone2 = clone2.twist(Twist.LEFT_180)
+        //tests counter-clockwise / L'
+        clone3 = clone3.twist(Twist.LEFT_270)
+        assertTrue(clone3 == clone2)
+        clone3 = clone3.twist(Twist.LEFT_270)
+        assertTrue(clone3 == clone1)
+        clone3 = clone3.twist(Twist.LEFT_270)
+        assertTrue(clone3 == start)
     }
     @Test
     fun rightTwists() {
-        //TODO(tuples)
+        //sets up and tests initial turns
+        val start = solved()
+        var clone1: Twistable = Cube(start)
+        var clone2: Twistable = Cube(start)
+        var clone3: Twistable = Cube(start)
+
+        clone1 = clone1.twist(Twist.RIGHT_90)
+        clone2 = clone2.twist(Twist.RIGHT_180)
+        clone3 = clone3.twist(Twist.RIGHT_270)
+        assertFalse(clone1 == start)
+        assertFalse(clone2 == start)
+        assertFalse(clone3 == start)
+
+        //tests clockwise / R
+        clone1 = clone1.twist(Twist.RIGHT_90)
+        assertTrue(clone1 == clone2)
+        clone1 = clone1.twist(Twist.RIGHT_90)
+        assertTrue(clone1 == clone3)
+        clone1 = clone1.twist(Twist.RIGHT_90)
+        assertTrue(clone1 == start)
+        clone1 = clone1.twist(Twist.RIGHT_90)
+        //tests half-turn / R2
+        clone2 = clone2.twist(Twist.RIGHT_180)
+        assertTrue(clone2 == start)
+        clone2 = clone2.twist(Twist.RIGHT_180)
+        //tests counter-clockwise / R'
+        clone3 = clone3.twist(Twist.RIGHT_270)
+        assertTrue(clone3 == clone2)
+        clone3 = clone3.twist(Twist.RIGHT_270)
+        assertTrue(clone3 == clone1)
+        clone3 = clone3.twist(Twist.RIGHT_270)
+        assertTrue(clone3 == start)
     }
     @Test
     fun upTwists() {
-        //TODO(tuples)
+        //sets up and tests initial turns
+        val start = solved()
+        var clone1: Twistable = Cube(start)
+        var clone2: Twistable = Cube(start)
+        var clone3: Twistable = Cube(start)
+
+        clone1 = clone1.twist(Twist.UP_90)
+        clone2 = clone2.twist(Twist.UP_180)
+        clone3 = clone3.twist(Twist.UP_270)
+        assertFalse(clone1 == start)
+        assertFalse(clone2 == start)
+        assertFalse(clone3 == start)
+
+        //tests clockwise / U
+        clone1 = clone1.twist(Twist.UP_90)
+        assertTrue(clone1 == clone2)
+        clone1 = clone1.twist(Twist.UP_90)
+        assertTrue(clone1 == clone3)
+        clone1 = clone1.twist(Twist.UP_90)
+        assertTrue(clone1 == start)
+        clone1 = clone1.twist(Twist.UP_90)
+        //tests half-turn / U2
+        clone2 = clone2.twist(Twist.UP_180)
+        assertTrue(clone2 == start)
+        clone2 = clone2.twist(Twist.UP_180)
+        //tests counter-clockwise / U'
+        clone3 = clone3.twist(Twist.UP_270)
+        assertTrue(clone3 == clone2)
+        clone3 = clone3.twist(Twist.UP_270)
+        assertTrue(clone3 == clone1)
+        clone3 = clone3.twist(Twist.UP_270)
+        assertTrue(clone3 == start)
     }
     @Test
     fun downTwists() {
-        //TODO(tuples)
+        //sets up and tests initial turns
+        val start = solved()
+        var clone1: Twistable = Cube(start)
+        var clone2: Twistable = Cube(start)
+        var clone3: Twistable = Cube(start)
+
+        clone1 = clone1.twist(Twist.DOWN_90)
+        clone2 = clone2.twist(Twist.DOWN_180)
+        clone3 = clone3.twist(Twist.DOWN_270)
+        assertFalse(clone1 == start)
+        assertFalse(clone2 == start)
+        assertFalse(clone3 == start)
+
+        //tests clockwise / D
+        clone1 = clone1.twist(Twist.DOWN_90)
+        assertTrue(clone1 == clone2)
+        clone1 = clone1.twist(Twist.DOWN_90)
+        assertTrue(clone1 == clone3)
+        clone1 = clone1.twist(Twist.DOWN_90)
+        assertTrue(clone1 == start)
+        clone1 = clone1.twist(Twist.DOWN_90)
+        //tests half-turn / D2
+        clone2 = clone2.twist(Twist.DOWN_180)
+        assertTrue(clone2 == start)
+        clone2 = clone2.twist(Twist.DOWN_180)
+        //tests counter-clockwise / D'
+        clone3 = clone3.twist(Twist.DOWN_270)
+        assertTrue(clone3 == clone2)
+        clone3 = clone3.twist(Twist.DOWN_270)
+        assertTrue(clone3 == clone1)
+        clone3 = clone3.twist(Twist.DOWN_270)
+        assertTrue(clone3 == start)
     }
 }
