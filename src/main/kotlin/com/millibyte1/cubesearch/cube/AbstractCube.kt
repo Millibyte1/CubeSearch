1bc487c64d4131313d516e1b90e299fb92fe2b27
Evan Hall <ebhall99@gmail.com>
2020-09-11 23:36:17 -0400

    A little more documentation

--- src/main/kotlin/com/millibyte1/cubesearch/cube/AbstractCube.kt
+++ src/main/kotlin/com/millibyte1/cubesearch/cube/AbstractCube.kt
@@ -1,7 +1,12 @@
 package com.millibyte1.cubesearch.cube
 
 /**
- * Interface with a single function, twist, which takes a Twist and applies it to this object
+ * Abstract class with a single abstract method, twist, which takes a Twist and applies it to this cube.
+ *
+ * @property data a minimal representation of the state of the cube
+ *
+ * @constructor constructs cube from a copy of [data]
+ * @param data the 6x9 array representing the desired cube. Format: (front, back, left, right, up, down)
  */
 abstract class AbstractCube<T : AbstractCube<T>>(data: Array<IntArray>) {
 
@@ -10,9 +15,16 @@ abstract class AbstractCube<T : AbstractCube<T>>(data: Array<IntArray>) {
     init {
         this.data = data.copy()
     }
+
+    /**
+     * copy constructor
+     * @param cube the cube to copy
+     */
+    constructor(cube: Cube) : this(cube.data) { }
     
     /**
-     * returns the cube resulting from applying the given twist
+     * returns the cube resulting from applying the given twist.
+     * Depending on implementation, may modify this object or return a new object
      *
      * @param twist the twist we are applying to this cube
      * @return the cube resulting from the applying given twist
